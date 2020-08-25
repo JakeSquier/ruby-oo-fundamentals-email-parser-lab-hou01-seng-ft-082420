1a276fa153d3d200f5a5f7415ca497c7a2f23400
Jacob Squier <jacob.squier@gmail.com>
2020-08-25 07:15:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,12 +3,14 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-attr_accessor :emails
+  attr_accessor :emails
 
-def initialize(email)
-  @emails = email
+  def initialize(email)
+    @emails = email
+  end
+
+  def parse
+    emails.delete(',').split.uniq
+  end
+  
 end
-
-def parse
-  emails.delete(',').split.uniq
-e
